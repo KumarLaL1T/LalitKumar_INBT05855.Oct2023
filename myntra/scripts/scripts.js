cc27f706a56f8da7bbab37c6515c767e5125f59a
lalit <lalitk2832002@gmail.com>
2023-11-05 12:53:01 +0530

    first commit

--- myntra/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    document.getElementById('loading-screen').style.opacity = '0';
-});
-
-window.addEventListener('load', function () {
-
-    document.getElementById('loading-screen').style.display = 'none';
-});
